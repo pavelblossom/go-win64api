26353069d6d3f6394c19c048cb5efb5135c5aa15
ptsvetkov <p.tsvetkov@it-expertise.ru>
2019-08-06 18:20:01 +0700

    added softinfo from user path

--- software.go
+++ software.go
@@ -4,35 +4,31 @@ package winapi
 
 import (
 	"fmt"
-	"time"
-
 	"golang.org/x/sys/windows/registry"
+	"time"
 
 	so "github.com/pavelblossom/go-win64api/shared"
 )
 
 func InstalledSoftwareList() ([]so.Software, error) {
-	_, os, _, _, _, err := GetSystemProfile()
+	var appSw []so.Software
+	sw64, err := getSoftwareList(`SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall`, "X64")
 	if err != nil {
 		return nil, err
 	}
-	if os.Architecture == "64-bit" {
-		sw64, err := getSoftwareList(`SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall`, "X64")
-		if err != nil {
-			return nil, err
-		}
-		sw32, err := getSoftwareList(`SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
-		if err != nil {
-			return nil, err
-		}
-		return append(sw64, sw32...), nil
-	} else {
-		sw32, err := getSoftwareList(`SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
-		if err != nil {
-			return nil, err
-		}
-		return sw32, nil
+	appSw = append(appSw, sw64...)
+	sw32, err := getSoftwareList(`SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
+	if err != nil {
+		return nil, err
 	}
+	appSw = append(appSw, sw32...)
+	swU, err := getUserSoftwareList(`Software\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
+	if err != nil {
+		return nil, err
+	}
+	appSw = append(appSw, swU...)
+
+	return appSw, nil
 }
 
 func InstalledSoftwareList32() ([]so.Software, error) {
@@ -40,9 +36,87 @@ func InstalledSoftwareList32() ([]so.Software, error) {
 	if err != nil {
 		return nil, err
 	}
-	return sw32, nil
+	swU, err := getUserSoftwareList(`Software\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
+	if err != nil {
+		return nil, err
+	}
+	return append(sw32, swU...), nil
 }
 
+func getUserSoftwareList(baseKey string, arch string) ([]so.Software, error) {
+	k, err := registry.OpenKey(registry.CURRENT_USER, baseKey, registry.QUERY_VALUE|registry.ENUMERATE_SUB_KEYS)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading from registry: %s", err.Error())
+	}
+	defer k.Close()
+
+	swList := make([]so.Software, 0)
+
+	subkeys, err := k.ReadSubKeyNames(-1)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading subkey list from registry: %s", err.Error())
+	}
+	for _, sw := range subkeys {
+		sk, err := registry.OpenKey(registry.CURRENT_USER, baseKey+`\`+sw, registry.QUERY_VALUE)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading from registry (subkey %s): %s", sw, err.Error())
+		}
+
+		dn, _, err := sk.GetStringValue("DisplayName")
+		if err == nil {
+			swv := so.Software{DisplayName: dn, Arch: arch}
+
+			dv, _, err := sk.GetStringValue("DisplayVersion")
+			if err == nil {
+				swv.DisplayVersion = dv
+			}
+
+			pub, _, err := sk.GetStringValue("Publisher")
+			if err == nil {
+				swv.Publisher = pub
+			}
+
+			id, _, err := sk.GetStringValue("InstallDate")
+			if err == nil {
+				swv.InstallDate, _ = time.Parse("20060102", id)
+			}
+
+			es, _, err := sk.GetIntegerValue("EstimatedSize")
+			if err == nil {
+				swv.EstimatedSize = es
+			}
+
+			cont, _, err := sk.GetStringValue("Contact")
+			if err == nil {
+				swv.Contact = cont
+			}
+
+			hlp, _, err := sk.GetStringValue("HelpLink")
+			if err == nil {
+				swv.HelpLink = hlp
+			}
+
+			isource, _, err := sk.GetStringValue("InstallSource")
+			if err == nil {
+				swv.InstallSource = isource
+			}
+
+			mver, _, err := sk.GetIntegerValue("VersionMajor")
+			if err == nil {
+				swv.VersionMajor = mver
+			}
+
+			mnver, _, err := sk.GetIntegerValue("VersionMinor")
+			if err == nil {
+				swv.VersionMinor = mnver
+			}
+
+			swList = append(swList, swv)
+		}
+	}
+
+	return swList, nil
+}
 func getSoftwareList(baseKey string, arch string) ([]so.Software, error) {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, baseKey, registry.QUERY_VALUE|registry.ENUMERATE_SUB_KEYS)
 	if err != nil {
